092577135747e5d77dd49f38f21310c2a98f4437
Muna Osman <mofadal009@gmail.com>
2025-10-10 16:34:35 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Greeting from "./components/greetings";
+import Header from "./components/Header";
+function App() {
+  return (
+    <>
+      <Greeting name = "Muna" surname = "Osman"/>
+      <Header />
+     
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/greetings.jsx
@@ -0,0 +1,10 @@
+// Greeting.jsx
+function Greeting(props) {
+  return <h2>Hello, {props.name} {props.surname} 👋</h2>;
+}
+
+export default Greeting;
+
+
+
+
